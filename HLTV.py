960fba42643ee6e4e6a819fece83ebbfbc35e4f3
Ralf <ralfleggett@gmail.com>
2021-12-08 17:44:20 +0000

    added map scraper

--- HLTV.py
+++ HLTV.py
@@ -42,7 +42,7 @@ class HLTV():
         for team in teams_html:
             name = team.div.find("div", {"class": "text-ellipsis"}).string
             id = re.split("/", team.a["href"])[2]
-            team_dict[name] = id
+            team_dict[id] = {"name": name}
 
         return team_dict
 
@@ -62,6 +62,50 @@ class HLTV():
         for player in players_html:
             name = player.a.div.string
             id = re.split("/", player.a["href"])[3]
-            players_dict[name] = {"id": id, "team_id": team_id}
+            players_dict[id] = {"name": name}
         
         return players_dict
+
+    def get_map_ids(self, player_ids, team_id, opponent_ids, 
+        latest_date=None, min_players=5):
+        """
+        Params:
+            player_ids:     list of ints. The 5 player ids that form team
+            team_id:        int. The id of the team the 5 players play for
+            opponent_ids:   list of ints. Team ids to get map ids for
+            latest_date:    date. Ignore maps (strictly) after this date
+            min_players:    int. How many of the player_ids to require to 
+                            include the map
+        Returns:
+            dictionary {(map_id: [team_id, opponent_id]
+        """
+        url = f"{self.base_url}/stats/lineup/matches?minLineupMatch={min_players}"
+        for id in player_ids:
+            url += f"&lineup={id}"
+        soup = self._soup_from_url(url)
+
+        maps_html = soup.find("table", {"class": "stats-table"}).tbody
+        maps_html = maps_html.find_all("tr")
+
+        map_ids = {}
+        for map in maps_html:
+            # Check date of map
+            date_td = map.td
+            date = datetime.strptime(date_td.a.string, "%d/%m/%y").date()
+            if latest_date is not None and date > latest_date:
+                continue
+            
+            # Get team IDs n.b. strings after an html tag count as the next #
+            # sibling, so double the .next_sibling count to account for the 
+            # new lines in the html document
+            team1_td = date_td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
+            team1_id = re.split("/", team1_td.a["href"])[3]
+            team2_id = re.split("/", team1_td.next_sibling.next_sibling.a["href"])[3]
+
+            # Append if team ids are what we're looking for
+            if team1_id == team_id and team2_id in opponent_ids:
+                map_id = re.split("/", date_td.a["href"])[4]
+                map_ids[map_id] = [team1_id, team2_id]
+
+        return map_ids
+
--- main.py
+++ main.py
@@ -1,36 +1,86 @@
+import collections
 import json
-import time
+
+from datetime import date
+from tqdm import tqdm
 
 from HLTV import HLTV
 
 MAJOR_EVENT_ID = 4866
+MAJOR_END_DATE = date(2021, 11, 7)
 
 def write_dict(dict_to_write, filename):
+    """
+    Writes a dictionary to the filename
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(dict_to_write, f, ensure_ascii=False, indent=4)
 
+def read_json(filename):
+    """
+    Reads a json file into a dictionary
+    """
+    with open(filename) as handle:
+        dictdump = json.loads(handle.read())
+    return dictdump
+
 def get_major_teams(hltv):
+    """
+    Queries HLTV for teams that played in final 16 of the 2021 PGL major
+    """
     return hltv.get_event_teams(MAJOR_EVENT_ID, "pgl-major-stockholm-2021")
 
 def get_major_players(hltv, team_dict):
+    """ 
+    Queries HLTV for players in teams in team_dict 
+    """
     players = {}
-    for team_name, team_id in team_dict.items():
-        print(team_name, team_id)
+    for team_id, team_name in tqdm(team_dict.items(), unit="team"):
         player_dict = hltv.get_event_team_players(team_id, team_name, MAJOR_EVENT_ID)
+        team_dict[team_id].update({"players": [id for id in player_dict]})
         players.update(player_dict)
-        for player_name, player_id in player_dict.items():
-            print(player_name, player_id)
-        print("--------------")
     return players
+
+def get_map_ids(hltv, team_dict, latest_date=None, min_players=5):
+    """
+    Gets all map ids between teams in team_dict where the players in the 
+    map were exactly the players specified in team_dict. Ignores maps
+    after latest_date if not None
+    Returns:
+        dictionary {map_id: [team1_id, team2_id]}
+    """
+    team_ids = list(team_dict.keys())
+    map_ids = {}            # IDs that have appeared at least once
+    confirmed_map_ids = {}  # IDs which have appeared for both teams
+
+    for team in tqdm(team_dict, unit="team"):
+        ids = hltv.get_map_ids(
+            team_dict[team]["players"], 
+            team, 
+            team_ids,
+            latest_date=latest_date,
+            min_players=min_players)
+        for id in ids:
+            if id not in map_ids:
+                map_ids.update({id: ids[id]})
+            else:
+                confirmed_map_ids.update({id: ids[id]})
+
+    return confirmed_map_ids
 
 def main():
     hltv = HLTV("hltv.org")
-    team_dict = get_major_teams(hltv)
-    player_dict = get_major_players(hltv, team_dict)
 
-    write_dict(team_dict, "teams.json")
-    write_dict(player_dict, "players.json")
-    
+    # team_dict = get_major_teams(hltv)
+    # player_dict = get_major_players(hltv, team_dict)
+    # map_ids = get_map_ids(hltv, team_dict, latest_date=MAJOR_END_DATE, min_players=4)
+    # write_dict(team_dict, "teams.json")
+    # write_dict(player_dict, "players.json")
+    # write_dict(map_ids, "maps_4_players.json")
 
+    team_dict = read_json("teams.json")
+    player_dict = read_json("players.json")
+
+  
 if __name__ == "__main__":
     main()
